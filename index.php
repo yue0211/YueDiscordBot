2ca16028d8aa6a833ab1cd593b0aafa7f6593428
yue0211 <82385589+yue0211@users.noreply.github.com>
2021-07-26 19:49:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 $text= $_POST["text"];
 // data strored in array
 
-$Json = file_get_contents("setting.json");
+/*$Json = file_get_contents("setting.json");
 // Converts to an array 
-$myarray = json_decode($Json, true);
+$myarray = json_decode($Json, true);*/
 
 
 //echo $text;
 $result="";
 
-for($i=0;$i<count($myarray["id"]);$i++)
+/*for($i=0;$i<count($myarray["id"]);$i++)
 	if($myarray["id"][$i]==$text)
 	{
 		for($j=0;$j<10;$j++)
@@ -26,6 +26,6 @@ $myarray["code"]=$result;
 
 // encode array to json
 $json = json_encode($myarray);
-$bytes = file_put_contents("setting.json", $json); 
+$bytes = file_put_contents("setting.json", $json); */
 
 ?>
