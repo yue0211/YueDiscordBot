136e9c63752e91d5ba1028b73b6120a7b96305ca
yue0211 <82385589+yue0211@users.noreply.github.com>
2021-07-27 20:46:53 +0800

    Update event.py

--- commands/event.py
+++ commands/event.py
@@ -12,9 +12,8 @@ class Event(Cog_Extension):
     @commands.Cog.listener() 
     async def on_member_join(self,member):  #成員加入頻道
         if member.guild.id==(int(data["Yue"])):
-            channel=self.bot.get_channel(int(data["YueMainRoom"])) #大廳
-            channelTwo=self.bot.get_channel(int(data["YueLevelUpRoom"])) #升級會員資格頻道
-            await channel.send(f'{member}已加入本群,如要解鎖更多頻道權限請去 {channelTwo.mention} 設定資料') 
+            channel=self.bot.get_channel(int(data["YueMainRoom"]))
+            await channel.send(f'{member}已加入本群,如要解鎖更多頻道權限請去 #升級會員資格 設定資料') 
             role = discord.utils.get(member.guild.roles,id=(int(data["YueNormalMember"]))) #普通會員身份
             await member.add_roles(role)
             await member.send(f"{member.name} 群內有許多資源,可善加利用,並遵守相關規定,謝謝配合")
@@ -46,8 +45,12 @@ class Event(Cog_Extension):
                     role=guild.get_role((int(data["YueRolePeko"]))) 
                     await payload.member.add_roles(role) 
                     await payload.member.send(f"你取得了 {role}身份組!!")
-       
-                
+                elif str(payload.emoji)==data["YueTalesRunner"]: 
+                    role=guild.get_role((int(data["YueRoleTalesRunner"]))) 
+                    await payload.member.add_roles(role) 
+                    await payload.member.send(f"你取得了 {role}身份組!!")
+
+
      # 取消反應貼圖取消身份組
     @commands.Cog.listener()                   
     async def on_raw_reaction_remove(self,payload): 
@@ -69,7 +72,14 @@ class Event(Cog_Extension):
                     member=guild.get_member(payload.user_id)
                     await member.remove_roles(role) 
                     await member.send(f"你移除了{role}身份組!!")
-        
+                elif str(payload.emoji)==data["YueTalesRunner"]:  
+                    role=guild.get_role((int(data["YueRoleTalesRunner"]))) 
+                    member=guild.get_member(payload.user_id)
+                    await member.remove_roles(role) 
+                    await member.send(f"你移除了{role}身份組!!")
+
+
+
 
 
 
